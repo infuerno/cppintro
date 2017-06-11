5abfbd5c9fbea0b5329bd40f88fbeafe1367c786
Claire Furney <infuerno@fastmail.fm>
2017-06-11 20:00:45 +0100

    Update makefiles and complete lecture3 ex1

---
+++ lecture3/convert/convert.cpp
@@ -0,0 +1,83 @@
+/* This program prints out a conversion table of temperatures, after
+   prompting the user for upper and lower bounds of the table in
+   Fahrenheit, and the temperature difference between table entries. */ 
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+double celsius_of(int fahr);
+double absolute_value_of(int fahr);
+void print_preliminary_message();
+void input_table_specifications(int& lower, int& upper, int& step);
+void print_message_echoing_input(int lower, int upper, int step);
+void print_table(int lower, int upper, int step);
+
+int main() 
+{	
+    int lower = 0; /* the lowest Fahrenheit entry in the table */
+    int upper = 0; /* the highest Fahrenheit entry in the table */
+    int step = 1;  /* difference in Fahrenheit between entries */
+
+    /* print a message explaining what the program does: */
+    print_preliminary_message();
+	
+    /* prompt the user for table specifications in Fahrenheit: */
+    input_table_specifications(lower, upper, step);
+	
+    /* print appropriate message including an echo of the input: */
+    print_message_echoing_input(lower, upper, step);
+	
+    /* Print the table (including the column headings): */
+    print_table(lower, upper, step);
+
+    return 0;
+}
+
+double celsius_of(int fahr)
+{
+    return (static_cast<double>(5)/9) * (fahr - 32);
+}
+
+double absolute_value_of(int fahr)
+{
+    return ((static_cast<double>(5)/9) * (fahr - 32)) + 273.15;
+}
+
+void print_preliminary_message()
+{
+    cout << "This program prints out a conversion table of temperatures." << endl;
+}
+
+void input_table_specifications(int& lower, int& upper, int& step)
+{
+    cout << "Enter the minimum (whole number) temperature you want in the table, in Fahrenheit: ";
+    cin >> lower;
+    cout << "Enter the maximum temperature you want in the table: ";
+    cin >> upper;
+    cout << "Enter the temperature difference you want between table entries: ";
+    cin >> step;
+}
+
+void print_message_echoing_input(int lower, int upper, int step)
+{
+    cout << "\nTemperature conversion table from " << lower << " Farenheit to " << upper << " Farenheit in steps of " << step << " Farenheit.\n\n";
+}
+
+void print_table(int lower, int upper, int step)
+{
+    cout << setiosflags (ios::left);
+    cout.width(20);
+    cout << "Farenheit";
+    cout.width(20);
+    cout << "Celsius" << "Absolute Value" << "\n";
+
+    cout.setf(ios::fixed);
+    cout.precision(2);
+    for (int farenheit = lower; farenheit <= upper; farenheit += step) {
+	cout.width(20);
+	cout << farenheit;
+	cout.width(20);
+	cout << celsius_of(farenheit) << absolute_value_of(farenheit) << "\n";
+    }
+}
